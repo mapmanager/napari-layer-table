d414b3eff6c267ca7ed6999f4e9b5ffb14808187
cudmore <robert.cudmore@gmail.com>
2022-03-09 09:54:58 -0800

    second commit

--- src/napari_layer_table/__init__.py
+++ src/napari_layer_table/__init__.py
@@ -6,3 +6,6 @@ from ._reader import napari_get_reader
 
 from ._sample_data import make_sample_data 
 from ._widget import ExampleQWidget, example_magic_widget
+
+# abb
+from ._my_widget import LayerTablePlugin
