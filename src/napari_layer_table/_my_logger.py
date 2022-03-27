0c0dc662c0168c22ef0407e5bd29d5ccc5ea4d82
Vasudha Jha <reachvasudha27@gmail.com>
2022-03-27 12:40:56 -0700

    store logging level in a variable

--- src/napari_layer_table/_my_logger.py
+++ src/napari_layer_table/_my_logger.py
@@ -1,12 +1,15 @@
 import logging
 import sys
 
+# default logging level
+logging_level = logging.INFO
+
 # This will create a custom logger with the name as the module name
 logger = logging.getLogger(__name__)
-logger.setLevel(logging.INFO)
+logger.setLevel(logging_level)
 
 handler = logging.StreamHandler(sys.stdout)
-handler.setLevel(logging.INFO)
+handler.setLevel(logging_level)
 formatter = logging.Formatter('%(asctime)s - %(levelname)5s - %(name)8s  %(filename)s %(funcName)s() line:%(lineno)d -- %(message)s')
 handler.setFormatter(formatter)
 
