1dddff5224984bc795ae8ee102e2099f1526cce2
Vasudha Jha <reachvasudha27@gmail.com>
2022-03-27 12:38:37 -0700

    fix logger

--- src/napari_layer_table/_my_logger.py
+++ src/napari_layer_table/_my_logger.py
@@ -1,9 +1,11 @@
 import logging
+import sys
 
 # This will create a custom logger with the name as the module name
 logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
 
-handler = logging.StreamHandler()
+handler = logging.StreamHandler(sys.stdout)
 handler.setLevel(logging.INFO)
 formatter = logging.Formatter('%(asctime)s - %(levelname)5s - %(name)8s  %(filename)s %(funcName)s() line:%(lineno)d -- %(message)s')
 handler.setFormatter(formatter)
