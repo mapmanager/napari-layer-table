7688bddc96fe89c520a02e35f431c8c2b7ff9e7b
Robert Cudmore <cudmore@Roberts-MacBook-Pro-2.local>
2022-04-17 19:02:01 -0700

    implement use in stackViewer

--- src/napari_layer_table/_my_logger.py
+++ src/napari_layer_table/_my_logger.py
@@ -10,7 +10,8 @@ logger.setLevel(logging_level)
 
 handler = logging.StreamHandler(sys.stdout)
 handler.setLevel(logging_level)
-formatter = logging.Formatter('%(asctime)s - %(levelname)7s - %(filename)s %(funcName)s() line:%(lineno)d -- %(message)s')
+#formatter = logging.Formatter('%(asctime)s - %(levelname)7s - %(filename)s %(funcName)s() line:%(lineno)d -- %(message)s')
+formatter = logging.Formatter('%(levelname)7s - %(filename)s %(funcName)s() line:%(lineno)d -- %(message)s')
 handler.setFormatter(formatter)
 
 logger.addHandler(handler)
