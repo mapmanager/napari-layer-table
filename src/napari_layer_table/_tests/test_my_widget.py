24599aae2cf7bd4c0f07c710aa327ed436e34983
Vasudha Jha <reachvasudha27@gmail.com>
2022-04-18 20:13:20 -0700

    fix test_getLayerDataframe_without_rowlist

--- src/napari_layer_table/_tests/test_my_widget.py
+++ src/napari_layer_table/_tests/test_my_widget.py
@@ -30,8 +30,8 @@ getLayerDataframe_with_rowlist_testcases = [
 ]
 
 getLayerDataframe_without_rowlist_testcases = [
-    (threeDimPoints, 'yellow', 'yellow triangles layer', '^', pd.DataFrame(np.array([["▲", 15, 66, 55, [1.0, 1.0, 0.0, 1.0]], ["▲", 15, 65, 60, [1.0, 1.0, 0.0, 1.0]], ["▲", 50, 85, 79, [1.0, 1.0, 0.0, 1.0]], ["▲", 20, 90, 68, [1.0, 1.0, 0.0, 1.0]]]), columns=["Symbol", "z", "x", "y", "Face Color"])),
-    (twoDimPoints, 'red', 'red triangles layer', '^', pd.DataFrame(np.array([["▲", 10, 55, [1.0, 0.0, 0.0, 1.0]], ["▲", 10, 65, [1.0, 0.0, 0.0, 1.0]], ["▲", 10, 75, [1.0, 0.0, 0.0, 1.0]], ["▲", 10, 85, [1.0, 0.0, 0.0, 1.0]]]), columns=["Symbol", "x", "y", "Face Color"]))
+    (threeDimPoints, 'yellow', 'yellow triangles layer', '^', pd.DataFrame(np.array([["▲", 0, 15, 66, 55, [1.0, 1.0, 0.0, 1.0]], ["▲", 1, 15, 65, 60, [1.0, 1.0, 0.0, 1.0]], ["▲", 2, 50, 85, 79, [1.0, 1.0, 0.0, 1.0]], ["▲", 3, 20, 90, 68, [1.0, 1.0, 0.0, 1.0]]]), columns=["Symbol", "rowIdx", "z", "x", "y", "Face Color"])),
+    (twoDimPoints, 'red', 'red triangles layer', '^', pd.DataFrame(np.array([["▲", 0, 10, 55, [1.0, 0.0, 0.0, 1.0]], ["▲", 1, 10, 65, [1.0, 0.0, 0.0, 1.0]], ["▲", 2, 10, 75, [1.0, 0.0, 0.0, 1.0]], ["▲", 3, 10, 85, [1.0, 0.0, 0.0, 1.0]]]), columns=["Symbol", "rowIdx", "x", "y", "Face Color"]))
 ]
 
 hideColumns_testcases = [
